32ba4fcc7ddbb817f70531cba83c58e0b82abd87
ExPeacer <kisbeke9@gmail.com>
2012-11-02 23:29:52 +0100

    Initial push

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+-include device/semc/mogami-common/BoardConfigCommon.mk
+-include vendor/semc/smultron/BoardConfigVendor.mk
+
+TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
+TARGET_KERNEL_CONFIG := pac_smultron_defconfig
+#TARGET_PREBUILT_KERNEL := device/semc/smultron/prebuilt/kernel
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-gnueabihf-linaro-4.7
+
+SENSORS_COMPASS_AK8975 := true
+SENSORS_ACCEL_BMA150_INPUT := false
+SENSORS_ACCEL_BMA250_INPUT := true
+SENSORS_PROXIMITY_APDS9700 := true
+SENSORS_PRESSURE_BMP180 := false
+
+TARGET_OTA_ASSERT_DEVICE := ST15i,ST15a,smultron
+
---
+++ smultron.mk
@@ -0,0 +1,43 @@
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/semc/smultron/smultron-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/semc/smultron/overlay
+
+$(call inherit-product, device/semc/mogami-common/mogami.mk)
+
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/smultron/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
+    device/semc/smultron/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
+    device/semc/smultron/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
+    device/semc/smultron/prebuilt/bootrec-device:root/sbin/bootrec-device \
+    device/semc/smultron/recovery.fstab:root/recovery.fstab
+
+# Device specific configs
+PRODUCT_COPY_FILES += \
+    device/semc/smultron/config/cyttsp-spi.idc:system/usr/idc/cyttsp-spi.idc \
+    device/semc/smultron/config/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
+    device/semc/smultron/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
+    device/semc/smultron/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
+    device/semc/smultron/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    device/semc/smultron/config/cyttsp_key.kl:system/usr/keylayout/cyttsp_key.kl \
+    device/semc/smultron/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/semc/smultron/config/sensors.conf:system/etc/sensors.conf \
+    device/semc/smultron/config/tiwlan.ini:system/etc/tiwlan.ini
+
+$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-mdpi.mk)
+
+#LCD DENSITY
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160 \
+    com.qc.hdmi_out=false
