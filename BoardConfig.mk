d7a68b04de193fa7ca4ce6d3ae04fecff4bd485d
codeworkx <codeworkx@cyanogenmod.com>
2012-11-14 15:45:49 +0100

    smultron: device specific part for the new two-stage boot method
    Ported from http://review.cyanogenmod.org/#/c/26440/
    Author: codeworkx <codeworkx@cyanogenmod.com>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ic9ad9fb4f6d700ac469bbe193176425b5b5dd6bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,8 @@ TARGET_KERNEL_CONFIG := pac_smultron_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/smultron/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-gnueabihf-linaro-4.7
 
+TARGET_RECOVERY_INITRC := device/semc/smultron/recovery/init.rc
+
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := false
 SENSORS_ACCEL_BMA250_INPUT := true
